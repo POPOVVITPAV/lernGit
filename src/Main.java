15e62f225e916ab9a8b77278bd5c34d06256ebbf
POPOVVITPAV <POPOVVITPAV@yandex.ru>
2021-05-31 21:49:16 +0300

    Первые измененния

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-            System.out.println("Lean Git");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input number");
+        int numIterator = sc.nextInt();
+        int[] arr = new int[numIterator];
+        for (int i=0; i<arr.length; i++){
+            System.out.println("Input integer number");
+            int numArray = sc.nextInt();
+            arr[i] = numArray;
+        }
+            System.out.println(arr.length);
         }
     }
 
